b59ee6a93e9b4102df5b19feef32ffda307b8b33
MIO1100 <melnichenko739@mail.ru>
2020-05-27 18:30:21 +0700

    I did connect with db and give information to site

--- attachment/views.py
+++ attachment/views.py
@@ -14,15 +14,26 @@ def index(request):
     cursor.execute("SELECT max(DATE) FROM db.`table`;")
     result = cursor.fetchall()[0][0]
     result = datetime.date(2020,4,28)+datetime.timedelta(days=1)
-    if (now>result):
+    if (now<result):
+        print("1")
         date_list = [result + datetime.timedelta(i) for i in range((now - result).days + 1)]
         response = requests.get("https://www.cbr-xml-daily.ru/daily_json.js")
         todos = json.loads(response.text)
         data= todos['Date'].split('T')[0]
         date = datetime.date(int(data.split('-')[0]), int(data.split('-')[1]), int(data.split('-')[2]))
         rate = Rates("",date, todos['Valute']['EUR']['Value'], todos['Valute']['USD']['Value'], todos['Valute']['JPY']['Value'], todos['Valute']['CNY']['Value'], todos['Valute']['EUR']['Nominal'], todos['Valute']['USD']['Nominal'], todos['Valute']['JPY']['Nominal'], todos['Valute']['CNY']['Nominal'])
-        cursor.execute(rate.insert()).fetchall()
+        cursor.execute(rate.insert())
 
 
+    s = cursor.execute("SELECT * FROM db.`table`")
+    print(s)
+    # for row in cursor:
+    #     print(row)
+    #
 
-    return HttpResponse("Hello, world. You're at the polls index." )
+
+    return render(
+        request,
+        'attachment/rates.html',
+        context={'data': cursor.description},
+    )
